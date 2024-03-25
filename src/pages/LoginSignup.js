5e5aad211db5a21bc1440613937a068ed8f37d25
Abhishekr2122 <abhishekrshinde21@gmail.com>
2024-03-25 23:18:55 +0530

    Adding the components to the loginsignup page.

--- src/pages/LoginSignup.js
+++ src/pages/LoginSignup.js
@@ -1,3 +1,31 @@
+import styled from "styled-components";
+
+const StyledConatiner = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: row;
+`;
+
+const StyledSubContainer1 = styled.div`
+  height: 100%;
+  width: 50%;
+`;
+
+const StyledSubContainer2 = styled.div`
+  height: 100%;
+  width: 50%;
+`;
+
+const StyledFormContainer = styled.div``;
+
 export default function LoginSignup() {
-  return <div>Its a login signup page</div>;
+  return (
+    <StyledConatiner>
+      <StyledSubContainer1>Its the LoginSignup page</StyledSubContainer1>
+      <StyledSubContainer2>
+        <StyledFormContainer></StyledFormContainer>
+      </StyledSubContainer2>
+    </StyledConatiner>
+  );
 }
