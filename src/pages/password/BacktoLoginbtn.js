c73da856a3d42d92f48edf2f7351f6385dbe4881
Abhishekr2122 <abhishekrshinde21@gmail.com>
2024-05-24 19:52:33 +0530

    Worked on the password page.

--- src/pages/password/BacktoLoginbtn.js
+++ src/pages/password/BacktoLoginbtn.js
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 import { IoMdArrowBack } from "react-icons/io";
-import { NavLink } from "react-router-dom";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Spinner from "../../ui/Spinner";
 
 const StyledBtn = styled.button`
   display: flex;
@@ -11,25 +13,45 @@ const StyledBtn = styled.button`
   width: 100%;
   border-style: none;
   cursor: pointer;
-  &:hover {
-    text-decoration: underline;
-    text-decoration-color: white;
-  }
+  text-decoration: underline;
+  color: white;
 `;
 
 const StyledHeading = styled.h3`
   color: white;
 `;
 
+const StyledLoginContainer = styled.div`
+  border: 1px solid white;
+  border-radius: 10px;
+`;
+
 export default function BacktoLoginbtn() {
+  const navigate = useNavigate();
+
+  const [isButtonClicked, setIsButtonClicked] = useState(false);
+
+  function handleButtonClick() {
+    setIsButtonClicked(true);
+    setTimeout(function () {
+      navigate("/login");
+      setIsButtonClicked(false);
+    }, 1100);
+  }
+
   return (
-    <NavLink to="/login" style={{ textDecoration: "none" }}>
+    <StyledLoginContainer onClick={handleButtonClick}>
       <StyledBtn>
-        <IoMdArrowBack
-          style={{ color: "white", height: "20px", width: "20px" }}
-        />
+        {isButtonClicked ? (
+          <Spinner />
+        ) : (
+          <IoMdArrowBack
+            style={{ color: "white", height: "20px", width: "20px" }}
+          />
+        )}
+
         <StyledHeading>Return to the login screen</StyledHeading>
       </StyledBtn>
-    </NavLink>
+    </StyledLoginContainer>
   );
 }
